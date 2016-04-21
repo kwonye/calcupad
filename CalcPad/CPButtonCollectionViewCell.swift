24b538f4c473fa5c0534bcc00a5a421e905feb3c
Will Kwon <will@kwon.co>
2016-04-20 21:55:55 -0700

    Adding CPCollectionViewCell that has button

---
+++ CalcPad/CPButtonCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CPButtonCollectionViewCell.swift
+//  CalcPad
+//
+//  Created by Will Kwon on 4/20/16.
+//  Copyright © 2016 Will Kwon. All rights reserved.
+//
+
+import UIKit
+
+class CPButtonCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var button: UIButton!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        button.setTitleColor(UIColor.purpleColor(), forState: UIControlState.Normal)
+    }
+}
