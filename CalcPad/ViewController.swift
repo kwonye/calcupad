498c01c6168b97e134b9187b91573ac853d34993
Will Kwon <will@kwon.co>
2016-04-24 14:10:24 -0700

    Removing all views and starting over with a horizontal stack view as the root view

--- CalcPad/ViewController.swift
+++ CalcPad/ViewController.swift
@@ -9,23 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var extraButtonsView: UIView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        toggleExtraButtonsView(self.view.frame.size)
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
-    }
-    
-    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
-        toggleExtraButtonsView(size)
-    }
-    
-    func toggleExtraButtonsView(size: CGSize) {
-        extraButtonsView.hidden = size.width > size.height
     }
 }
