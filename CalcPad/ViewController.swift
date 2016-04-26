3550fc8ef12b1885b6229efb25815aa4986341c3
Will Kwon <will@kwon.co>
2016-04-25 21:15:50 -0700

    Adding more buttons

--- CalcPad/ViewController.swift
+++ CalcPad/ViewController.swift
@@ -20,10 +20,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
-        
         buttons.append(1)
         buttons.append(2)
         buttons.append(3)
@@ -34,6 +30,19 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         buttons.append(8)
         buttons.append(9)
         buttons.append(0)
+        buttons.append(1)
+        buttons.append(2)
+        buttons.append(3)
+        buttons.append(4)
+        buttons.append(5)
+        buttons.append(6)
+        buttons.append(7)
+        buttons.append(8)
+        buttons.append(9)
+        buttons.append(0)
+        
+        super.viewDidLoad()
+    }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return sizeOfCell()
