d7dcbcf3688a3a08dfcb0a41fe686a8802ad9fdc
Will Kwon <will@kwon.co>
2016-05-05 20:21:46 -0700

    Adding types to CPButton

--- CalcPad/CPButton.swift
+++ CalcPad/CPButton.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class CPButton: NSObject {
-    enum CPButtonType {
+public class CPButton: NSObject {
+    let equals = "="
+    let period = "."
+    let operators = ["÷", "×", "+", "-"]
+    let modifiers = ["⁺∕₋", "%"]
+    let clear = ["C", "AC"]
+    
+    public enum CPButtonType {
+        case Clear
+        case Period
         case Number
         case Operator
         case Equals
         case Modifier
+    }
+    
+    var text: String = ""
+    var type: CPButtonType = .Number
+    
+    override init() {
+    }
+    
+    init(text: String) {
+        self.text = text
+        super.init()
+        setType()
+    }
+    
+    convenience init(int: Int) {
+        self.init(text: String(int))
+    }
+    
+    func setType() {
+        if Int(text) != nil {
+            type = .Number
+        } else if text == equals {
+            type = .Equals
+        } else if text == period {
+            type = .Period
+        }else if modifiers.contains(text) {
+            type = .Modifier
+        } else if clear.contains(text.uppercaseString) {
+            type = .Clear
+        }
     }
 }
