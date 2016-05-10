0797a3bbd312fb0ac700d2b88bb270bb040d7521
Will Kwon <will@kwon.co>
2016-05-09 21:58:10 -0700

    Using CPUIButton for UIButton with data

--- CalcPad/CPButtonCollectionViewCell.swift
+++ CalcPad/CPButtonCollectionViewCell.swift
@@ -9,5 +9,5 @@
 import UIKit
 
 class CPButtonCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var button: CPUIButton!
 }
---
+++ CalcPad/CPUIButton.swift
@@ -0,0 +1,13 @@
+//
+//  CPUIButton.swift
+//  CalcPad
+//
+//  Created by Will Kwon on 5/9/16.
+//  Copyright © 2016 Will Kwon. All rights reserved.
+//
+
+import UIKit
+
+class CPUIButton: UIButton {
+    var data: CPButton?
+}
