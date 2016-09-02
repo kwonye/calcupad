2fdd1de61046f364b6bdf8448e25356aa4aa84f7
Will Kwon <will@kwon.co>
2016-09-01 22:36:59 -0700

    Adding Calculator.swift for calculations

---
+++ Calcupad/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  Calcupad
+//
+//  Created by Will Kwon on 9/1/16.
+//  Copyright © 2016 Will Kwon. All rights reserved.
+//
+
+import UIKit
+
+class Calculator: NSObject {
+
+}
