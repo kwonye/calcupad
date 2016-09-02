7e9e8f7872b920a6540ba11514740eba1405435a
Will Kwon <will@kwon.co>
2016-09-01 22:42:13 -0700

    Testing solveEquation with first nil parameter

--- CalcupadTests/CalculatorTests.swift
+++ CalcupadTests/CalculatorTests.swift
@@ -7,29 +7,22 @@
 //
 
 import XCTest
+@testable import Calcupad
 
 class CalculatorTests: XCTestCase {
+    var testCalculator: Calculator?
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testCalculator = Calculator()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testCalculator = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSolveEquationNilFirstValueReturnsNil() {
+        XCTAssertNil(testCalculator?.solveEquation(nil, secondValue: 1, currentOperator: "+"))
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
