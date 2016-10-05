b9b6c4055b3f605fa93de16376896e1b9cc1458a
Will Kwon <will@kwon.co>
2016-10-05 15:41:12 -0700

    Removing useless comparable

--- Calcupad/ViewController.swift
+++ Calcupad/ViewController.swift
@@ -8,26 +8,6 @@
 
 import UIKit
 import CoreData
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l > r
-  default:
-    return rhs < lhs
-  }
-}
-
 
 class ViewController: UIViewController {
     @IBOutlet weak var divideButton: CalculatorButton!
