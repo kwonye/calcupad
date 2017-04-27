94d9bb285b32aa58ad9ff54cd1e19c4b9eb2d928
Will Kwon <will@kwon.co>
2017-04-26 20:42:49 -0700

    Removing optional calculator

--- CalcupadTests/CalculatorTests.swift
+++ CalcupadTests/CalculatorTests.swift
@@ -10,39 +10,38 @@ import XCTest
 @testable import Calcupad
 
 class CalculatorTests: XCTestCase {
-    var testCalculator: Calculator?
+    var testCalculator = Calculator()
     
     override func setUp() {
         super.setUp()
-        testCalculator = Calculator()
     }
     
     override func tearDown() {
-        testCalculator = nil
         super.tearDown()
     }
     
     func testSolveEquationNilFirstValueReturnsNil() {
-        XCTAssertNil(testCalculator!.solveEquation(nil, secondValue: 1, currentOperator: "+"))
+        XCTAssertNil(testCalculator.solveEquation(nil, secondValue: 1, currentOperator: "+"))
     }
     
     func testSolveEquationNilSecondValueReturnsNil() {
-        XCTAssertNil(testCalculator!.solveEquation(1, secondValue: nil, currentOperator: "+"))
+        XCTAssertNil(testCalculator.solveEquation(1, secondValue: nil, currentOperator: "+"))
     }
     
     func testSolveEquationNilOperatorReturnsNil() {
-        XCTAssertNil(testCalculator!.solveEquation(1, secondValue: 2, currentOperator: nil))
+        XCTAssertNil(testCalculator.solveEquation(1, secondValue: 2, currentOperator: nil))
     }
     
     func testSolveEquationUnknownOperatorReturnsNil() {
-        XCTAssertNil(testCalculator!.solveEquation(1, secondValue: 2, currentOperator: "hey"))
+        XCTAssertNil(testCalculator.solveEquation(1, secondValue: 2, currentOperator: "hey"))
     }
     
     func testSolveEquationOnePlusTwoReturnsThree() {
-        XCTAssertEqual(testCalculator!.solveEquation(1, secondValue: 2, currentOperator: testCalculator!.plus), 3)
+        XCTAssertEqual(testCalculator.solveEquation(1, secondValue: 2, currentOperator: testCalculator.plus), 3)
     }
     
     func testSolveEquationOneMinusTwoReturnsNegativeOne() {
-        XCTAssertEqual(testCalculator!.solveEquation(1, secondValue: 2, currentOperator: testCalculator!.minus), -1)
+        XCTAssertEqual(testCalculator.solveEquation(1, secondValue: 2, currentOperator: testCalculator.minus), -1)
+    }
     }
 }
